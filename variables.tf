2f7ed9d661e4be8b0dac2dfdade4605c14ec3e21
otroka@gmail.com <otroka@gmail.com>
2023-07-02 09:27:28 +0000

    tf created

---
+++ variables.tf
@@ -0,0 +1,53 @@
+variable "GOOGLE_PROJECT" {
+  type        = string
+  default = "kbot-389211"
+  description = "GCP project name"
+}
+           
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = "us-central1-c"
+  description = "GCP region to use"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  description = "Number of nodes in GKE"
+  default     = 2
+}
+
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+  default     = "g1-small"
+  description = "Machine type"
+}
+
+variable "algorithm" {
+  type        = string
+  default     = "ECDSA"
+  description = "The cryptographic algorithm (e.g. RSA, ECDSA)"
+}
+
+variable "ecdsa_curve" {
+  type        = string
+  default     = "P256"
+  description = "The elliptic curve (e.g. P256, P384, P521)"
+}
+
+variable "GITHUB_OWNER" {
+  type = string
+  default = "otrokov"
+  description = "Github username"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type = string
+  default = "flux-gitops"
+  description = "GitHub repository to store Flux manifests"
+}
+
+variable "GITHUB_TOKEN" {
+  type = string
+  description = "The token used to authenticate with the Git repository"
+}
+
---
+++ vars.tfvars
@@ -0,0 +1,3 @@
+FLUX_GITHUB_REPO = "flux-gitops"
+GITHUB_OWNER     = "otrokov"
+GOOGLE_PROJECT   = "kbot-389211"
